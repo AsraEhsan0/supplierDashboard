b01d5960b17129bfdbabe053407aade9da80d729
Asra <214556539+AsraEhsan0@users.noreply.github.com>
2025-09-03 13:56:09 +0500

    _

--- SupplierDashboard/obj/Debug/net8.0/SupplierDashboard.AssemblyInfo.cs
+++ SupplierDashboard/obj/Debug/net8.0/SupplierDashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupplierDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a3d3bb212f1f3cbd4339aa6efac2dead8f64c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupplierDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupplierDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
