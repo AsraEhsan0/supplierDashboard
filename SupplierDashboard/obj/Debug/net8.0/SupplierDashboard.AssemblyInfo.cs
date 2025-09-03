78382441ccd9e10e4c9c75cb14f07955df540c91
Asra <214556539+AsraEhsan0@users.noreply.github.com>
2025-09-03 13:50:33 +0500

    add .gitignore file

--- SupplierDashboard/obj/Debug/net8.0/SupplierDashboard.AssemblyInfo.cs
+++ SupplierDashboard/obj/Debug/net8.0/SupplierDashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupplierDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c14cf5ce2009ffe9f37dea0b670c5c701e2ee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupplierDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupplierDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
